f9575de42a1951017db44fcf07584b4ef143b397
Julian Mueller <julian.mu.mueller@daimler.com>
2021-12-24 17:20:42 +0100

    add threed_dimensional_position class to obtain 3D pos for each label

--- python/load_dtld.py
+++ python/load_dtld.py
@@ -10,6 +10,8 @@ import numpy as np
 import cv2
 from dtld_parsing.calibration import CalibrationData
 from dtld_parsing.driveu_dataset import DriveuDatabase
+from dtld_parsing.three_dimensional_position import ThreeDimensionalPosition
+
 
 __author__ = "Andreas Fregin, Julian Mueller and Klaus Dietmayer"
 __maintainer__ = "Julian Mueller"
@@ -23,8 +25,7 @@ np.set_printoptions(suppress=True)
 logging.basicConfig(
     stream=sys.stdout,
     level=logging.INFO,
-    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: "
-    "%(message)s",
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: " "%(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
@@ -32,20 +33,16 @@ logging.basicConfig(
 def parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--label_file",
-                        help="DTLD label files (.json)",
-                        type=str,
-                        required=True)
-    parser.add_argument("--calib_dir",
-                        help="calibration directory where .yml are stored",
-                        type=str,
-                        required=True)
-    parser.add_argument("--data_base_dir",
-                        default="",
-                        help="only use this if the image file paths in the"
-                        "label files are not up to date. Do NOT change the"
-                        "internal DTLD folder structure!",
-                        type=str)
+    parser.add_argument("--label_file", help="DTLD label files (.json)", type=str, required=True)
+    parser.add_argument("--calib_dir", help="calibration directory where .yml are stored", type=str, required=True)
+    parser.add_argument(
+        "--data_base_dir",
+        default="",
+        help="only use this if the image file paths in the"
+        "label files are not up to date. Do NOT change the"
+        "internal DTLD folder structure!",
+        type=str,
+    )
     return parser.parse_args()
 
 
@@ -57,21 +54,23 @@ def main(args):
         return False
 
     # Load calibration
-    calibration = CalibrationData()
-    intrinsic_left = calibration.load_intrinsic_matrix(
-        args.calib_dir + "/intrinsic_left.yml"
-    )
-    rectification_left = calibration.load_rectification_matrix(
-        args.calib_dir + "/rectification_left.yml"
-    )
-    projection_left = calibration.load_projection_matrix(
-        args.calib_dir + "/projection_left.yml"
-    )
-    extrinsic = calibration.load_extrinsic_matrix(
-        args.calib_dir + "/extrinsic.yml"
-    )
-    distortion_left = calibration.load_distortion_matrix(
-        args.calib_dir + "/distortion_left.yml"
+    calibration_left = CalibrationData()
+    intrinsic_left = calibration_left.load_intrinsic_matrix(args.calib_dir + "/intrinsic_left.yml")
+    rectification_left = calibration_left.load_rectification_matrix(args.calib_dir + "/rectification_left.yml")
+    projection_left = calibration_left.load_projection_matrix(args.calib_dir + "/projection_left.yml")
+    extrinsic = calibration_left.load_extrinsic_matrix(args.calib_dir + "/extrinsic.yml")
+    distortion_left = calibration_left.load_distortion_matrix(args.calib_dir + "/distortion_left.yml")
+
+    calibration_right = CalibrationData()
+    projection_right = calibration_right.load_projection_matrix(args.calib_dir + "/projection_right.yml")
+
+    threed_position = ThreeDimensionalPosition(
+        calibration_left=calibration_left,
+        calibration_right=calibration_right,
+        binning_x=2,
+        binning_y=2,
+        roi_offset_x=0,
+        roi_offset_y=0,
     )
 
     logging.info("Intrinsic Matrix:\n\n{}\n".format(intrinsic_left))
@@ -85,9 +84,13 @@ def main(args):
 
     # Visualize image by image
     for idx_d, img in enumerate(database.images):
+
         # Get disparity image
         img_disp = img.visualize_disparity_image()
-        rects = img.map_labels_to_disparity_image(calibration)
+        rects = img.map_labels_to_disparity_image(calibration_left)
+
+        disparity_image = img.get_disparity_image()
+
         # Plot labels into disparity image
         for rect in rects:
             cv2.rectangle(
@@ -100,6 +103,17 @@ def main(args):
         # Get color image with labels
         img_color = img.get_labeled_image()
         img_color = cv2.resize(img_color, (1024, 440))
+
+        # Demo how to get 3D position
+        for o in img.objects:
+            # Camera Coordinate System: X right, Y down, Z front
+            threed_pos = threed_position.determine_three_dimensional_position(
+                o.x, o.y, o.width, o.height, disparity_image=disparity_image
+            )
+            # Get in vehicle coordinates: X front, Y left and Z up
+            threed_pos_numpy = np.array([threed_pos.get_pos()[0], threed_pos.get_pos()[1], threed_pos.get_pos()[2], 1])
+            threed_pos_vehicle_coordinates = extrinsic.dot(threed_pos_numpy)
+            print("TL position (rear axle): ", threed_pos_vehicle_coordinates)
         # Plot side by side
         img_concat = np.concatenate((img_color, img_disp), axis=1)
         # Because of the weird qt error in gui methods in opencv-python >= 3
